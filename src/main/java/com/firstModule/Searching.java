6c74a9869b1656db17bef6dc4411ecbcc1d6a58a
Gautam Deka <de.gautam19@gmail.com>
2019-02-24 11:34:58 +0530

    Initial Commit

---
+++ src/main/java/com/firstModule/Searching.java
@@ -0,0 +1,66 @@
+package com.firstModule;
+
+import java.util.Scanner;
+
+public class Searching {
+
+	public static void main(String[] args) {
+		//System.out.println(linearSearch());
+		binarySearch();
+	}
+
+	/**
+	 * program for linear search
+	 * @return true if number is found else false
+	 */
+	static boolean linearSearch() {
+		int[] nums = { 2, 4, 6, 3, 4, 5 };
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter no to search ");
+		int num = sc.nextInt();
+
+		for (int n : nums) {
+			if (n == num)
+				return true;
+		}
+		return false;
+	}
+	
+	static void binarySearch() {
+		int[] nums = {2,4,6,8};
+		
+		int item, location = -1;		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number to search !");
+		item = sc.nextInt();
+		
+		location = binarySearchItem(nums,0,nums.length,item);
+		if(location != -1)
+			System.out.println("Found at "+location);
+		else 
+			System.out.println("Not Found");
+	}
+
+	/**
+	 * @param nums
+	 * @param beg
+	 * @param end
+	 * @param item
+	 * @return position of the number
+	 */
+	private static int binarySearchItem(int[] nums, int beg, int end, int item) {
+		int mid;
+		
+		if(end >= beg) {
+			mid = (end+beg)/2;
+			System.out.println("Mid "+mid);
+			if(nums[mid] == item)
+				return mid+1;
+			else if(nums[mid]<item)
+				return binarySearchItem(nums, mid+1, end, item);
+			else
+				return binarySearchItem(nums, beg, mid-1, item);
+		}		
+		return -1;
+	}
+}
---
+++ src/main/java/com/firstModule/Sorting.java
@@ -0,0 +1,12 @@
+package com.firstModule;
+
+public class Sorting {
+	
+	public static void main(String[] args) {
+		
+	}
+
+	void BubbleSort() {
+		
+	}
+}
